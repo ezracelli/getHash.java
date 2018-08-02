98ead50be4693de12ab43b810f6467769a76362d
Ezra Celli <ezra@Temperance.local>
2018-08-02 13:39:08 -0400

    Add effective comments

--- getHash.java
+++ getHash.java
@@ -6,6 +6,11 @@ import java.nio.file.Files;
 
 public class getHash {
 
+    /**
+     * Calulates the hash value
+     * @param inputBytes Bytes from input file
+     * @param algorithm Algorithm to be used
+     */
     public static String hash(byte[] inputBytes, String algorithm){
         String hashValue = null;
 
@@ -22,9 +27,14 @@ public class getHash {
         return hashValue;
     }
 
+    /**
+     * Main method
+     */
     public static void main(String[] args) {
+        // Open a scanner
         Scanner s = new Scanner(System.in);
 
+        // Prompt the user for algorithm type and file location
         System.out.println("\nChoose from MD2, MD5, SHA-1, SHA-224, SHA-256, " +
             "SHA-384, SHA-512.\n");
         System.out.print("Algorithm: ");
@@ -32,13 +42,31 @@ public class getHash {
         System.out.print("Input file: ");
         String inputFile = s.nextLine();
 
+        // Try to open the input file
         try {
+            // Try to get byte array from input file
             File file = new File(inputFile);
             byte[] fileBytes = Files.readAllBytes(file.toPath());
-            System.out.println("\nHash: " + hash(fileBytes, algorithm));
+            s.close();
+
+            // Final printout
+            String fileHash = hash(fileBytes, algorithm);
+
+            // Ensure hash() returned something
+            if (fileHash != null) {
+                System.out.println("\nHash: " + fileHash + "\n");
+            } else {
+                System.out.println("\nError: Hash algorithm does not exist!\n");
+            }
         }
         catch(Exception e){
+            System.out.println("\nError: Input file does not exist!\n");
+            s.close();
 
+            // Exit
+            System.exit(1);
         }
+
+        
     }
 }
